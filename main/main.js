c0cd50531f6347281e10b26073e70d110ddcb914
Dima Zhadan <109739013+ZDimaO@users.noreply.github.com>
2022-08-03 16:04:21 +0300

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,3 +1,4 @@
+//hamburger menu
 const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
    const menuList = document.querySelector('.header__nav');
@@ -5,5 +6,28 @@ if (iconMenu) {
       document.body.classList.toggle('_lock')
       iconMenu.classList.toggle('_activete');
       menuList.classList.toggle('_activete');
+   })
+}
+
+//scroll up btn
+
+const upBtn = document.getElementById('upbtn')
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      upBtn.style.display = "block";
+   } else {
+      upBtn.style.display = "none";
+   }
+}
+
+if (upBtn) {
+   upBtn.addEventListener('click', function (e) {
+      window.scrollTo({
+         top: 0,
+         behavior: "smooth"
+      });
    })
 }
