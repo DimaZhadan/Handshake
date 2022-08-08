abfa982d46bc8f7685f63196c711c9dd936369b8
Dima Zhadan <109739013+DimaZhadan@users.noreply.github.com>
2022-08-08 13:23:16 +0300

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -88,3 +88,31 @@ dots.forEach((item, indexDot) => {
 
 window.setInterval(nextslide, 9000);
 
+//sviper
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+
+   slidesPerView: 6,
+   spaceBetween: 10,
+   // Responsive breakpoints
+   breakpoints: {
+     // when window width is >= 320px
+     320: {
+       slidesPerView: 2,
+       spaceBetween: 20
+     },
+     // when window width is >= 480px
+     480: {
+       slidesPerView: 3,
+       spaceBetween: 30
+     },
+     // when window width is >= 640px
+     640: {
+       slidesPerView: 4,
+       spaceBetween: 40
+     }
+   }
+ });
